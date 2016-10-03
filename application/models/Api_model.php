b1b06ea65a510c2b785d0c3beea92591a0f7299e
unknown <rita_miffies@yahoo.co.id>
2016-10-03 21:43:09 +0700

    nama barang

--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -372,7 +372,7 @@ class Api_model extends CI_Model {
         $this->db->select();
         $this->db->from('histori');
         $this->db->join('barang', 'histori.id_barang=barang.id', 'left');
-        $this->db->where('id_barang', $id);
+        $this->db->where('nama', $id);
         $query = $this->db->get();
 
         if ($query == null) {
